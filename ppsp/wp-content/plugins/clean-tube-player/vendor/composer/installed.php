36b0823551be61078b372f9d2a860067e5bff436
DESKTOP-BBK2RFG\Administrator <lux8pw@gmail.com>
2021-12-11 23:16:30 +0800

    no message

---
+++ ppsp/wp-content/plugins/clean-tube-player/vendor/composer/installed.php
@@ -0,0 +1,53 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fc8a2868c0961c861fceb9b5197d38eb390aab34',
+        'name' => 'wp-script/clean-tube-player',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'campo/random-user-agent' => array(
+            'pretty_version' => '1.3.0',
+            'version' => '1.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../campo/random-user-agent',
+            'aliases' => array(),
+            'reference' => '4a88bd90f66ca398b050b19848e3c6ea143ce8a0',
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'wp-script/clean-tube-player' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fc8a2868c0961c861fceb9b5197d38eb390aab34',
+            'dev_requirement' => false,
+        ),
+    ),
+);
